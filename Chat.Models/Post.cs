36b4d9c747fb8e9e704d13134d764a264f3e3860
velialarm <velialarm@gmail.com>
2014-09-06 13:02:25 +0300

    Add default project schema

---
+++ Chat.Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Chat.Models
+{
+    public class Post
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PostOn { get; set; }
+
+        public User PostedBy { get; set; }
+    }
+
+}
---
+++ Chat.Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Chat.Models
+{
+    public class User
+    {
+        public User(string name)
+        {
+            this.Name = name;
+            this.LoggedTimeFrom = DateTime.Now;
+        }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime LoggedTimeFrom { get; set; }
+    }
+}
